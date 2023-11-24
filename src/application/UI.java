e95fb046334ba763ae1f04adf580cb36c624ec60
edsonwisses <wisses@gmail.com>
2023-11-24 15:31:13 -0300

    Limpando a tela e exceções

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,6 +29,11 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 		String s = sc.nextLine();
@@ -52,12 +57,6 @@ public class UI {
 	}
 
 	private static void printPiece(ChessPiece piece) {
-		if (piece == null) {
-			System.out.print("-");
-		} else {
-			System.out.print(piece);
-		}
-		System.out.print(" ");
 		if (piece == null) {
 			System.out.print("-");
 		} else {
