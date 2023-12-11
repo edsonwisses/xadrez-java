fa4d0a3cdf54b71c7de480d08411f7611d47d6ca
edsonwisses <wisses@gmail.com>
2023-12-11 19:22:26 -0300

    Logica do checkmate

--- src/application/UI.java
+++ src/application/UI.java
@@ -55,9 +55,15 @@ public class UI {
 		 printCapturedPieces(captured);
 		 System.out.println();
 		 System.out.println("Vez : "+chessMatch.getTurn());
-		 System.out.println("Esperando jogador: "+chessMatch.getCurrentPlayer());
-		 if(chessMatch.getCheck()) {
-			 System.out.println("Check!");
+		 if(!chessMatch.getCheckMate()) {
+			 System.out.println("Esperando jogador: "+chessMatch.getCurrentPlayer());
+			 if(chessMatch.getCheck()) {
+				 System.out.println("Check!");
+			 }
+		 }
+		 else {
+			 System.out.println("CHECKMATE!");
+			 System.out.println("Vencedor: "+chessMatch.getCurrentPlayer());
 		 }
 	 }
 	public static void printBoard(ChessPiece[][] pieces) {
